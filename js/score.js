30b23c5770b03fb106c2c9a5d1fa8d73bced58a4
Endres <andresdhr99@gmail.com>
2021-06-29 19:08:45 -0400

    Added comments to files explaining functionality and sections

--- js/score.js
+++ js/score.js
@@ -1,8 +1,10 @@
+//Gets ul element from scores.html
 var scoreList = document.getElementById("player-list") 
 
 var renderScores = function(){
-   var scoreOfPlayer = JSON.parse(window.localStorage.getItem("scores")|| []);
-    console.log(scoreOfPlayer);
+    //Parses and gets array of player objacts
+    var scoreOfPlayer = JSON.parse(window.localStorage.getItem("scores")|| []);
+    //For each element in the array create an h3 add the contents of the player object and append them to the page
     scoreOfPlayer.forEach(function(score){
         var h3 = document.createElement("h3");
         h3.textContent = score.name + " score: "+ score.endScore;
